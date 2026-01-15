ec162701f29ba22fc793d16c5cc69090e586b5d8
Bovin <160007558+vvineey@users.noreply.github.com>
2026-01-15 11:20:16 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] argc) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A + B);
+
+    }
+
+}
