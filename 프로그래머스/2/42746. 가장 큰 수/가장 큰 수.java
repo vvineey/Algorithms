b451dd1aa78f4352d50a54b703a2c2a795054239
Bovin <160007558+vvineey@users.noreply.github.com>
2026-05-20 14:22:09 +0900

    [level 2] Title: 가장 큰 수, Time: 127.77 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+
+class Solution {
+    public String solution(int[] numbers) {
+     
+        String[] arr = new String[numbers.length];
+        
+        for (int i = 0;i < numbers.length;i++){
+            arr[i] = String.valueOf(numbers[i]);
+      
+        }
+        
+        Arrays.sort(arr,(o1,o2) -> (o2+o1).compareTo(o1+o2));
+        
+        if (arr[0].equals("0")){
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String c : arr){
+            sb.append(c);
+        }
+        
+        
+        
+        return sb.toString();
+    }
+}
