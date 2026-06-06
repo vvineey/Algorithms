1eecec8af5cd4740c6f5485804fd4b0a2a01744c
Bovin <160007558+vvineey@users.noreply.github.com>
2026-06-07 01:36:41 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.92 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -0,0 +1,14 @@
+def solution(intStrs, k, s, l):
+    answer = []
+    
+    for i in intStrs:
+        tmp = i[s:s+l]
+        
+        print(tmp)
+        
+        if (int(tmp) > k) :
+            answer.append(int(tmp))
+        
+    
+    
+    return answer
