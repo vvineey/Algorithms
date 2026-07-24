f873ab58f1336c198bf49cb8a6ffb4af907451cb
Bovin <160007558+vvineey@users.noreply.github.com>
2026-07-24 13:27:54 +0900

    [level 2] Title: 프로세스, Time: 6.77 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,35 +3,67 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         
-        //인덱스, 우선순위
+        //위치, 우선순위
         Deque<int[]> wait = new ArrayDeque<>();
-        ArrayList<Integer> answer = new ArrayList<>();
-    
-        for (int i = 0; i < priorities.length; i++){
-            wait.addLast(new int[]{i,priorities[i]});
+        int n = priorities.length;
+        
+        
+        //0번 프로세스 넣기이
+        wait.offer(new int[]{0,priorities[0]});
+        
+        
+        System.out.println("현재 대기 큐 상태");
+        for (int[] k : wait){
+            System.out.println(k[0] + " " + k[1]);
         }
         
         
-        for (int[] value :wait){
-            System.out.println(value[0] + " "+ value[1]);
-        }
-        
-        while (!wait.isEmpty()){
+        for (int i = 1; i < n;i++){
+            int current = priorities[i];
             
-            //최댓값 찾기
-            int maxNum = 0;
-            for (int[] value : wait){
-                maxNum = Math.max(maxNum, value[1]);
+            System.out.println("==========");
+            System.out.println("i " + i + " current " + current + "\n현재 대기 큐 상태");
+            for (int[] k : wait){
+                System.out.println(k[0] + " " + k[1]);
             }
+
+            wait.offer(new int[]{i,current});
             
-            //top이 최댓값이 아니면 뽑아서 맨 뒤에 넣음 
-            while (wait.peek()[1] != maxNum){
-                wait.addLast(wait.poll());
+            int cnt = wait.size();
+            while (cnt >= 0) {
+                
+                int[] top = wait.poll();
+                
+                if (top[1] <= current) {
+
+                    System.out.println(top[1] + " vs " + current);
+                    wait.offer(top);
+                }
+                else{
+                     wait.addLast(top);
+                }
+                cnt--;
             }
-            
-            answer.add(wait.poll()[0]);
+ 
         }
         
-        return answer.indexOf(location) +1;
+        
+        System.out.println("==========");
+        int answer = 0;
+        int cnt = 0;
+        
+        
+        System.out.println("마지막 대기 큐 상태\n ");
+        for (int[] i : wait){
+            cnt++;
+            System.out.println(i[0] + " " + i[1]);
+            if (i[0] == location){
+                answer = cnt;
+            }
+        }
+        
+        System.out.println("==========");
+
+        return answer;
     }
 }
