66ae7310e834f4801265057c007f5652ac528a52
Bovin <160007558+vvineey@users.noreply.github.com>
2026-08-07 16:01:01 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 10.22 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -26,6 +26,13 @@ class Solution {
     
     public int solution(int[][] jobs) {
         
+        //요청 시간 단위로 정렬 
+        for (int [] job : jobs){
+            Arrays.sort(jobs, (o1,o2)->{
+                return Integer.compare(o1[0],o2[0]);
+            });
+        }
+        
         //우선순위 : 소요시간 > 요청 시각 > 인덱스 
         PriorityQueue<Request> queue = new PriorityQueue<>((o1,o2)->{
             if (o1.workTime != o2.workTime){
@@ -38,47 +45,38 @@ class Solution {
             return Integer.compare(o1.idx, o2.idx);
         });
         
-        Queue<Work> hardDisk = new ArrayDeque<>();
         
         int current = 0;
         int cnt = 0;
         int total = 0;
+        int jobIdx = 0;
 
         while (cnt < jobs.length){
             // System.out.println("시점 " + current);
             
-            for (int i = 0; i < jobs.length; i++){
-                
-                //현재 시점 == 작업 요청 시각 -> 대기큐에 넣음
-                if (current == jobs[i][0]){
-                    // System.out.println("작업 요청 " + i + " " + jobs[i][0] + " " + jobs[i][1]);
-                    queue.offer(new Request(i,jobs[i][0],jobs[i][1]));
-                    jobs[i][0] = -1;
-                }
+            
+            //현재 시점 == 작업 요청 시각 -> 대기큐에 넣음
+            while(jobIdx < jobs.length && current >= jobs[jobIdx][0]){
+                    // System.out.println("작업 요청 " + jobIdx + " " + jobs[jobIdx][0] + " " + jobs[jobIdx][1]);
+                    queue.offer(new Request(jobIdx,jobs[jobIdx][0],jobs[jobIdx][1]));
+                    jobIdx++;
             }
             
-            //작업 중이지 않으면 대기큐에서 우선순위가 높은 작업을 빼서 시작함
-            if (hardDisk.isEmpty() && !queue.isEmpty()){
-                Request request = queue.poll();
-                // System.out.println("작업 시작 " + request.idx + " " + request.requestTime + " " + request.workTime);
-                hardDisk.offer(new Work (request, current));
+            //대기 중인 작업이 없는 경우 -> 다음 작업 요청 시간으로 이동 
+            if (queue.isEmpty()){
+                current = jobs[jobIdx][0];
+                continue;
             }
             
-           if (!hardDisk.isEmpty()) {
-                Work work = hardDisk.peek();
-                if (current == (work.start + work.request.workTime)){
-                    // System.out.println("작업 끝 " + work.request.idx + " " + work.request.requestTime + " " + work.request.workTime);
-                    hardDisk.poll();
-                    cnt++;
-                    
-                    // System.out.println("반환시간 " + (current - work.request.requestTime));
-                    total += (current - work.request.requestTime);
-                    continue;
-                }
-            }
+            //대기 중인 작업이 있는 경우 -> 작업 실행 후 시간을 종료 시간으로 이동
+            Request request = queue.poll();
             
+            current += request.workTime;
+            total += (current - request.requestTime);
+            // System.out.println("반환 시간 " + (current - request.requestTime));
             
-            current++;
+            cnt++;
+           
         }
         
 
